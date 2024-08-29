f5df889e9b3ff17fab15c1007f4edc1d5a574889
ManoUPDev <emanuelcgalindo@hotmail.com>
2024-08-29 19:36:50 -0300

    Commit 2.4

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Ex1 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+        
+        if (numero > 10) {
+            System.out.println("Número maior do que 10!");
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+        }
+
+        scanner.close();
+    }
 }
