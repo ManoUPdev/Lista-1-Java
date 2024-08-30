829e0269d6ebee313004d0b401415e44dd239705
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-08-30 10:59:43 -0300

    3 commit

--- src/Ex10.java
+++ src/Ex10.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class Ex10 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero de 1 a 5: ");
+        int numero = scanner.nextInt();
+
+        switch (numero) {
+            case 1:
+                System.out.println("Um");
+                break;
+            case 2:
+                System.out.println("Dois");
+                break;
+            case 3:
+                System.out.println("Três"); // Added accent
+                break;
+            case 4:
+                System.out.println("Quatro");
+                break;
+            case 5:
+                System.out.println("Cinco"); // Changed to println for consistency
+                break;
+            default:
+                System.out.println("Número inválido!"); // Added accent
+        }
+
+        scanner.close();
+    }
 }
--- src/Ex9.java
+++ src/Ex9.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Ex9 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero (A): ");
+        int a = scanner.nextInt();
+        System.out.println("Digite o segundo numero (B): ");
+        int b = scanner.nextInt();
+
+        
+        if (a == b) {
+            System.out.println("Os numeros sao iguais.");
+        } else if (a > b) {
+            System.out.println("Os numeros sao diferentes. O maior e A: " + a);
+        } else {
+            System.out.println("Os numeros sao diferentes. O maior e B: " + b);
+        }
+
+        scanner.close();
+    }
 }
